e43a9281ba5cc6368065b3027eaf3e2a319737f0
Terryn Brunelle <terryn@studioterryn.com>
2022-05-04 20:22:35 -0400

    skeleton for register

--- functions.py
+++ functions.py
@@ -1,5 +1,21 @@
-def register(username, password):
+# maps usernames to ENPs
+authDB = {}
 
+
+def register(username, password):
+  if username in authDB:
+    # TODO: raise a better error
+    raise Error("username already exists")
+
+  hashedPass = getHash(password)
+  negPass = getNegPass(hashedPass)
+  enp = encrypt(hashedPass, negPass) # probably replace this with python library encrypt func
+
+  authDB[username] = enp
 
 
 def login(username, password):
+  if username not in authDB:
+    raise Error(f"noo account for uesrname {username} exists")
+
+
