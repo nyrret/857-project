4a93c38706faeb4863a45078b6798003caf770de
Terryn Brunelle <terryn@studioterryn.com>
2022-05-04 21:23:53 -0400

    encryption aes

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Requirements
 ==============
 
 - bistring
+- cryptography
--- functions.py
+++ functions.py
@@ -1,5 +1,8 @@
-import hashlib
 from bitstring import BitArray
+import hashlib
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.fernet import Fernet
 
 # maps usernames to ENPs
 authDB = {}
@@ -10,9 +13,11 @@ def register(username, password):
     # TODO: raise a better error
     raise Error("username already exists")
 
-  hashedPass = _getHash(password)
-  negPass = _getNegPass(hashedPass)
-  enp = _encrypt(hashedPass, negPass) # probably replace this with python library encrypt func
+  hashedPassHex = _getHash(password)
+  # hashedPassBits = BitArray(hex=hashedPassHex).bin[2:] # strip leading 0b
+
+  negPass = _getNegPass(hashedPassBits)
+  enp = _encrypt(hashedPassHex.encode('utf-8'), negPass) # probably replace this with python library encrypt func
 
   authDB[username] = enp
 
@@ -35,10 +40,31 @@ def login(username, password):
 # ============ Internal Functions ==============
 
 def _getHash(password):
-  m = hashlib.sha256()
+  # m = hashlib.sha256()
+  
+  m = hashlib.sha1()  # 128 bits
   m.update(password.encode("utf-8"))
-  hashedHex = m.hexdigest()
 
-  # convert to bitstring before returning
-  return BitArray(hex=hashedHex).bin[2:]  # strip leading 0b
+  return m.digest()
 
+
+# key -- in bytes
+def _encryptAES(key, plaintext):
+  # 128-bit encryption
+  import base64
+  f = Fernet(base64.urlsafe_b64encode(key))
+  ct = f.encrypt(plaintext)
+  
+
+  # TODO -- 256
+  # iv = os.urandom(16)
+  # cipher = Cipher(algorithms.AES(key), modes.CBC(iv)) 
+  # encryptor = cipher.encryptor()
+  # ct = encryptor.update(plaintext.encode('utf-8')) + encryptor.finalize()
+
+  return ct
+
+
+def _decryptAES(key, ct):
+  decryptor = cipher.decryptor()
+  return decryptor.udpate(ct) + decryptor.finalize()
