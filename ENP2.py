d75a6a7be0bda27483457176a83cbcefcf0ef338
Winston Fee <winston.f.321@gmail.com>
2022-05-08 19:56:06 -0400

    added verifiaction

--- ENP2.py
+++ ENP2.py
@@ -46,16 +46,69 @@ def login(username, password):
 
 
 # ============ Internal Functions ==============
-def isSolution(hashedPass, negPass):
+def isSolutionEasy(hashedPass, negPass):
   for entry in negPass:
-    # if (len(entry) != len(negPass)):
-    #   print("wrong length")
-    #   return False
-    # for i in range(len(entry)):
-    #   if (entry[i] != '*' and entry[i] != hashedPass[i]):
-      if matches(hashedPass, entry):
-        return False
+    if matches(hashedPass, entry):
+      return False
   return True
+
+def isSolution(hashedPass, negPass):
+  m = len(hashedPass)
+  for i in range(m+4):
+    if (numberOfSp(negPass[i]) != 3):
+      return False
+  if (numberOfDS(negPass[m-2], negPass[m-1]) != 1 
+    or numberOfDS(negPass[m], negPass[m+1]) != 1 
+    or numberOfDS(negPass[m+2], negPass[m+3]) != 1):
+    return False
+  negPass[m-2] = merge(negPass[m-2], negPass[m-1])
+  negPass[m] = merge(negPass[m], negPass[m+1])
+  negPass[m+2] = merge(negPass[m+2], negPass[m+3])
+  if (numberOfDS(negPass[m], negPass[m+2])) != 1:
+      return False
+  negPass[m-1] = merge(negPass[m], negPass[m+2])
+  x = list(' '*len(hashedPass))
+  for i in range(m, -1, -1):
+      if numberOfSp(negPass[i]) != 1:
+          return False
+      k = indexOfSp(negPass[i])
+      x[k] = '0' if entry[k] == '1' else '1'
+      for j in range(i-1, -1,-1):
+        if negPass[j][k] != x[k] or negPass[j][k] != '*':
+          return False
+        negPass[j] = negPass[j][:k]+'*'+negPass[j][k+1:]
+  if "".join(x) == hashedPass:
+    return True
+  return False
+
+def numberOfSp(entry):
+  return len(entry) - entry.count('*')
+
+def indexOfSp(entry):
+  onePos = entry.find('1')
+  if onePos > -1:
+    return onePos
+  return entry.find('0')
+
+def numberOfDS(entry1, entry2):
+  if len(entry1) != len(entry2):
+    return False
+  count = 0
+  for i in range(len(entry1)):
+    if entry1[i] != entry2[i]:
+        count+=1
+  return count
+
+def merge(entry1, entry2):
+  if len(entry1) != len(entry2):
+    return False
+  ret = ''
+  for i in range(len(entry1)):
+      if entry1[i] == entry2[i]:
+          ret+=entry1[i]
+      else:
+          ret+= '*'
+  return ret
 
 def matches(hashedPass, databaseEntry):
 	if len(hashedPass) != len(databaseEntry):
