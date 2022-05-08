1fb3d242e405e086e0ddc23613fc0611563b5e20
Peter Rowley <rowley@mit.edu>
2022-05-08 19:28:16 -0400

    implemented second negative password algorithm

--- ENP2.py
+++ ENP2.py
@@ -6,10 +6,12 @@ from cryptography.fernet import Fernet, InvalidToken
 import numpy as np
 import base64
 import pickle
+from numpy.random import default_rng
 
 # maps usernames to ENPs
 authDB = {}
 
+rng = default_rng()
 
 def register(username, password):
   if username in authDB:
@@ -58,12 +60,12 @@ def isSolution(hashedPass, negPass):
   return True
 
 def matches(hashedPass, databaseEntry):
-	if len(hashedPass) != len(databaseEntry):
-		return False
-	for i in range(len(hashedPass)):
-		if databaseEntry[i] != "*" and databaseEntry[i] != hashedPass[i]:
-			return False
-	return True
+  if len(hashedPass) != len(databaseEntry):
+    return False
+  for i in range(len(hashedPass)):
+    if databaseEntry[i] != "*" and databaseEntry[i] != hashedPass[i]:
+      return False
+  return True
 
 def getHash(password):
   m = hashlib.sha256()
@@ -99,12 +101,49 @@ def decryptAES(key, ct):
   return pickle.loads(f.decrypt(ct))
 
 def getNegPass(hashedPass):
-	permutation = np.random.permutation(len(hashedPass))
-	permutedPass = permute(hashedPass, permutation)
-	negativeDB=[]
-	for i in range(len(permutedPass)):
-		negativeDB.append(inversePermute(permutedPass[:i]+opposite(permutedPass[i]) + "*"*(len(permutedPass)-i-1), permutation))
-	return negativeDB
+  permutation = np.random.permutation(len(hashedPass))
+  permutedPass = permute(hashedPass, permutation)
+  print(permutedPass)
+  negativeDB=[]
+  for i in range(len(permutedPass)-1, 1, -1): #One of these steps works if the first two bits agree
+    j, k = rng.integers(low=0, high=i, size=2)
+    while (j == k):
+      j, k = rng.integers(low=0, high=i, size=2)
+    if(j < k):
+      toAdd = "*" * j + permutedPass[j] + "*" * (k-j-1) + permutedPass[k] + "*" * (i-k-1) + opposite(permutedPass[i]) + "*" * (len(permutedPass)-i-1)
+    if(k < j):
+      toAdd = "*" * k + permutedPass[k] + "*" * (j-k-1) + permutedPass[j] + "*" * (i-j-1) + opposite(permutedPass[i]) + "*" * (len(permutedPass)-i-1)
+    negativeDB.append(inversePermute(toAdd, permutation))
+
+  j = rng.integers(low = 2, high = len(permutedPass)) #If the second bit disagrees but the first bit agrees
+  StartOfToAdd= permutedPass[0] + opposite(permutedPass[1]) + "*" * (j-2)
+  negativeDB.append(inversePermute(StartOfToAdd + str(0) + "*" * (len(permutedPass)-j-1), permutation))
+  negativeDB.append(inversePermute(StartOfToAdd + str(1) + "*" * (len(permutedPass)-j-1), permutation))
+
+  j, k = rng.integers(low = 1, high = len(permutedPass), size = 2) # if the first bit disagrees and the jth bit is 0
+  while(j == k):
+    j, k = rng.integers(low = 1, high = len(permutedPass), size = 2)
+  if (j < k):
+    StartOfToAdd = opposite(permutedPass[0]) + "*" * (j-1) + str(0) + "*" * (k-j-1)
+    negativeDB.append(inversePermute(StartOfToAdd + str(0) + "*" * (len(permutedPass)-k-1), permutation))
+    negativeDB.append(inversePermute(StartOfToAdd + str(1) + "*" * (len(permutedPass)-k-1), permutation))
+  if(k < j):
+    StartOfToAdd = opposite(permutedPass[0]) + "*" * (k-1)
+    negativeDB.append(inversePermute(StartOfToAdd + str(0) + "*" * (j-k-1) + str(0) + "*" * (len(permutedPass)-j-1), permutation))
+    negativeDB.append(inversePermute(StartOfToAdd + str(1) + "*" * (j-k-1) + str(0) +  "*" * (len(permutedPass)-j-1), permutation))
+
+  k = rng.integers(low = 1, high = len(permutedPass)) # if the first bit disagrees and the jth bit is 1
+  while (j == k):
+    k = rng.integers(low = 1, high = len(permutedPass))
+  if (j < k):
+    StartOfToAdd = opposite(permutedPass[0]) + "*" * (j-1) + str(1) + "*" * (k-j-1)
+    negativeDB.append(inversePermute(StartOfToAdd + str(0) + "*" * (len(permutedPass)-k-1), permutation))
+    negativeDB.append(inversePermute(StartOfToAdd + str(1) + "*" * (len(permutedPass)-k-1), permutation))
+  if(k < j):
+    StartOfToAdd = opposite(permutedPass[0]) + "*" * (k-1)
+    negativeDB.append(inversePermute(StartOfToAdd + str(0) + "*" * (j-k-1) + str(1) + "*" * (len(permutedPass)-j-1), permutation))
+    negativeDB.append(inversePermute(StartOfToAdd + str(1) + "*" * (j-k-1) + str(1) + "*" * (len(permutedPass)-j-1), permutation))
+  return negativeDB
 
 def opposite(bit):
 	if bit=="1":
@@ -118,7 +157,12 @@ def permute(bitString, permutation):
 	return permutedString
 
 def inversePermute(permutedString, permutation):
-	bitString=""
-	for i in range(len(permutedString)):
-		bitString += permutedString[np.nonzero(permutation==i)[0][0]]
-	return bitString
+  return permutedString
+	# bitString=""
+	# for i in range(len(permutedString)):
+	# 	bitString += permutedString[np.nonzero(permutation==i)[0][0]]
+	# return bitString
+
+
+db = getNegPass("1100101100001")
+print(db)
